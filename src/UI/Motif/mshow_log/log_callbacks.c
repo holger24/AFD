5a9ff9b304751613b4388d31d13c429065ec90b8
Holger Kiehl (idefix) <Holger.Kiehl@dwd.de>
2017-09-26 16:20:17 +0200

    Fix a buffer overflow in show_log Dialogs.

--- src/UI/Motif/mshow_log/log_callbacks.c
+++ src/UI/Motif/mshow_log/log_callbacks.c
@@ -1,6 +1,6 @@
 /*
  *  log_callbacks.c - Part of AFD, an automatic file distribution program.
- *  Copyright (c) 1996 - 2015 Holger Kiehl <Holger.Kiehl@dwd.de>
+ *  Copyright (c) 1996 - 2017 Holger Kiehl <Holger.Kiehl@dwd.de>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -277,6 +277,8 @@ search_text(Widget w, XtPointer client_data, XtPointer call_data)
          last_pos = 0;
       }
    }
+   XtFree(text_str);
+   XtFree(search_str);
 
    return;
 }
