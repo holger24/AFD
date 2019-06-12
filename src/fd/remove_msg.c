3ab223003eebdd998e7e29c7399212f2165319b6
Holger Kiehl (idefix) <Holger.Kiehl@dwd.de>
2019-06-12 15:37:50 +0200

    Revert "If remove_only flag is set for function remove_msg(), do not decrease"
    This reverts commit faba1721cdc0593141548fcb958f50ccc52a928d.

--- src/fd/remove_msg.c
+++ src/fd/remove_msg.c
@@ -61,7 +61,7 @@ void
 remove_msg(int qb_pos, int remove_only)
 {
    if ((fra != NULL) && (qb[qb_pos].special_flag & FETCH_JOB) &&
-       (qb[qb_pos].pos < no_of_dirs) && (remove_only != YES))
+       (qb[qb_pos].pos < no_of_dirs))
    {
       /* Dequeue in FRA. */
       fra[qb[qb_pos].pos].queued -= 1;
@@ -71,46 +71,49 @@ remove_msg(int qb_pos, int remove_only)
          fra[qb[qb_pos].pos].queued = 0;
       }
 
-      if (fra[qb[qb_pos].pos].error_counter > 0)
+      if (remove_only != YES)
       {
-         lock_region_w(fra_fd,
-#ifdef LOCK_DEBUG
-                       (char *)&fra[qb[qb_pos].pos].error_counter - (char *)fra, __FILE__, __LINE__);
-#else
-                       (char *)&fra[qb[qb_pos].pos].error_counter - (char *)fra);
-#endif
-         fra[qb[qb_pos].pos].error_counter = 0;
-         if (fra[qb[qb_pos].pos].dir_flag & DIR_ERROR_SET)
+         if (fra[qb[qb_pos].pos].error_counter > 0)
          {
-            fra[qb[qb_pos].pos].dir_flag &= ~DIR_ERROR_SET;
-            SET_DIR_STATUS(fra[qb[qb_pos].pos].dir_flag,
-                           time(NULL),
-                           fra[qb[qb_pos].pos].start_event_handle,
-                           fra[qb[qb_pos].pos].end_event_handle,
-                           fra[qb[qb_pos].pos].dir_status);
-            error_action(fra[qb[qb_pos].pos].dir_alias, "start",
-                         DIR_ERROR_ACTION);
-            event_log(0L, EC_DIR, ET_EXT, EA_ERROR_START, "%s",
-                      fra[qb[qb_pos].pos].dir_alias);
-         }
-         unlock_region(fra_fd,
+            lock_region_w(fra_fd,
 #ifdef LOCK_DEBUG
-                       (char *)&fra[qb[qb_pos].pos].error_counter - (char *)fra, __FILE__, __LINE__);
+                          (char *)&fra[qb[qb_pos].pos].error_counter - (char *)fra, __FILE__, __LINE__);
 #else
-                       (char *)&fra[qb[qb_pos].pos].error_counter - (char *)fra);
+                          (char *)&fra[qb[qb_pos].pos].error_counter - (char *)fra);
 #endif
-      }
+            fra[qb[qb_pos].pos].error_counter = 0;
+            if (fra[qb[qb_pos].pos].dir_flag & DIR_ERROR_SET)
+            {
+               fra[qb[qb_pos].pos].dir_flag &= ~DIR_ERROR_SET;
+               SET_DIR_STATUS(fra[qb[qb_pos].pos].dir_flag,
+                              time(NULL),
+                              fra[qb[qb_pos].pos].start_event_handle,
+                              fra[qb[qb_pos].pos].end_event_handle,
+                              fra[qb[qb_pos].pos].dir_status);
+               error_action(fra[qb[qb_pos].pos].dir_alias, "start",
+                            DIR_ERROR_ACTION);
+               event_log(0L, EC_DIR, ET_EXT, EA_ERROR_START, "%s",
+                         fra[qb[qb_pos].pos].dir_alias);
+            }
+            unlock_region(fra_fd,
+#ifdef LOCK_DEBUG
+                          (char *)&fra[qb[qb_pos].pos].error_counter - (char *)fra, __FILE__, __LINE__);
+#else
+                          (char *)&fra[qb[qb_pos].pos].error_counter - (char *)fra);
+#endif
+         }
 
-      /* Calculate the next scan time. */
-      if (fra[qb[qb_pos].pos].no_of_time_entries > 0)
-      {
-         fra[qb[qb_pos].pos].next_check_time = calc_next_time_array(fra[qb[qb_pos].pos].no_of_time_entries,
-                                                                    &fra[qb[qb_pos].pos].te[0],
+         /* Calculate the next scan time. */
+         if (fra[qb[qb_pos].pos].no_of_time_entries > 0)
+         {
+            fra[qb[qb_pos].pos].next_check_time = calc_next_time_array(fra[qb[qb_pos].pos].no_of_time_entries,
+                                                                       &fra[qb[qb_pos].pos].te[0],
 #ifdef WITH_TIMEZONE
-                                                                    fra[qb[qb_pos].pos].timezone,
+                                                                       fra[qb[qb_pos].pos].timezone,
 #endif
-                                                                    time(NULL),
-                                                                    __FILE__, __LINE__);
+                                                                       time(NULL),
+                                                                       __FILE__, __LINE__);
+         }
       }
    }
    if (*no_msg_queued > 0)
