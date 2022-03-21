ed51be28b725c14e9a33e8761b741ee96b04b7c1
Holger Kiehl (bb8) <Holger.Kiehl@dwd.de>
2022-03-21 12:39:01 +0100

    In search box of dir_ctrl dialog, add choice for protocol.

--- src/UI/Motif/dir_ctrl/select_dir_dialog.c
+++ src/UI/Motif/dir_ctrl/select_dir_dialog.c
@@ -1,6 +1,6 @@
 /*
  *  select_dir_dialog.c - Part of AFD, an automatic file distribution program.
- *  Copyright (c) 2003 - 2016 Holger Kiehl <Holger.Kiehl@dwd.de>
+ *  Copyright (c) 2003 - 2022 Holger Kiehl <Holger.Kiehl@dwd.de>
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -39,6 +39,7 @@ DESCR__S_M3
  **
  ** HISTORY
  **   20.12.2003 H.Kiehl Created
+ **   21.03.2022 H.Kiehl Added choice for protocol.
  **
  */
 DESCR__E_M3
@@ -74,15 +75,18 @@ extern struct fileretrieve_status *fra;
 
 /* Local global variables. */
 static Widget                     alias_toggle_w,
-                                  find_text_w;
+                                  find_text_w,
+                                  proto_togglebox_w;
 static int                        deselect,
                                   dirname_type,
                                   static_select;
+static XT_PTR_TYPE                toggles_set;
 
 /* Local function prototypes. */
 static void                       done_button(Widget, XtPointer, XtPointer),
                                   search_select_dir(Widget, XtPointer, XtPointer),
-                                  select_callback(Widget, XtPointer, XtPointer);
+                                  select_callback(Widget, XtPointer, XtPointer),
+                                  toggled(Widget, XtPointer, XtPointer);
 
 #define STATIC_SELECT_CB          1
 #define DESELECT_CB               2
@@ -110,6 +114,7 @@ select_dir_dialog(Widget w, XtPointer client_data, XtPointer call_data)
                       main_form_w,
                       radiobox_w,
                       separator_w,
+                      toggle_w,
                       togglebox_w;
       Arg             args[MAXARGS];
       Cardinal        argcount;
@@ -269,6 +274,83 @@ select_dir_dialog(Widget w, XtPointer client_data, XtPointer call_data)
       XtManageChild(separator_w);
 
       /*---------------------------------------------------------------*/
+      /*                        Protocol Selection                     */
+      /*---------------------------------------------------------------*/
+      proto_togglebox_w = XtVaCreateWidget("proto_togglebox",
+                                xmRowColumnWidgetClass, criteriabox_w,
+                                XmNorientation,         XmHORIZONTAL,
+                                XmNpacking,             XmPACK_TIGHT,
+                                XmNspacing,             0,
+                                XmNnumColumns,          1,
+                                XmNtopAttachment,       XmATTACH_WIDGET,
+                                XmNtopWidget,           separator_w,
+                                XmNleftAttachment,      XmATTACH_FORM,
+                                XmNresizable,           False,
+                                NULL);
+      toggle_w = XtVaCreateManagedWidget("FTP",
+                                xmToggleButtonGadgetClass, proto_togglebox_w,
+                                XmNfontList,               p_fontlist,
+                                XmNset,                    True,
+                                NULL);
+      XtAddCallback(toggle_w, XmNvalueChangedCallback,
+                    (XtCallbackProc)toggled, (XtPointer)SHOW_FTP);
+      toggle_w = XtVaCreateManagedWidget("HTTP",
+                                xmToggleButtonGadgetClass, proto_togglebox_w,
+                                XmNfontList,               p_fontlist,
+                                XmNset,                    True,
+                                NULL);
+      XtAddCallback(toggle_w, XmNvalueChangedCallback,
+                 (XtCallbackProc)toggled, (XtPointer)SHOW_HTTP);
+      toggle_w = XtVaCreateManagedWidget("LOC",
+                                xmToggleButtonGadgetClass, proto_togglebox_w,
+                                XmNfontList,               p_fontlist,
+                                XmNset,                    True,
+                                NULL);
+      XtAddCallback(toggle_w, XmNvalueChangedCallback,
+                    (XtCallbackProc)toggled, (XtPointer)SHOW_FILE);
+      toggle_w = XtVaCreateManagedWidget("EXEC",
+                                xmToggleButtonGadgetClass, proto_togglebox_w,
+                                XmNfontList,               p_fontlist,
+                                XmNset,                    True,
+                                NULL);
+      XtAddCallback(toggle_w, XmNvalueChangedCallback,
+                    (XtCallbackProc)toggled, (XtPointer)SHOW_EXEC);
+      toggle_w = XtVaCreateManagedWidget("SFTP",
+                                xmToggleButtonGadgetClass, proto_togglebox_w,
+                                XmNfontList,               p_fontlist,
+                                XmNset,                    True,
+                                NULL);
+      XtAddCallback(toggle_w, XmNvalueChangedCallback,
+                    (XtCallbackProc)toggled, (XtPointer)SHOW_SFTP);
+      XtManageChild(proto_togglebox_w);
+
+      toggles_set = SHOW_FTP |
+                    SHOW_HTTP |
+                    SHOW_SFTP |
+                    SHOW_FILE |
+                    SHOW_EXEC;
+
+      /*---------------------------------------------------------------*/
+      /*                      Horizontal Separator                     */
+      /*---------------------------------------------------------------*/
+      argcount = 0;
+      XtSetArg(args[argcount], XmNorientation,      XmHORIZONTAL);
+      argcount++;
+      XtSetArg(args[argcount], XmNtopAttachment,    XmATTACH_WIDGET);
+      argcount++;
+      XtSetArg(args[argcount], XmNtopWidget,        proto_togglebox_w);
+      argcount++;
+      XtSetArg(args[argcount], XmNtopOffset,        5);
+      argcount++;
+      XtSetArg(args[argcount], XmNleftAttachment,   XmATTACH_FORM);
+      argcount++;
+      XtSetArg(args[argcount], XmNrightAttachment,  XmATTACH_FORM);
+      argcount++;
+      separator_w = XmCreateSeparator(criteriabox_w, "separator", args,
+                                      argcount);
+      XtManageChild(separator_w);
+
+      /*---------------------------------------------------------------*/
       /*                         Select Box                            */
       /*---------------------------------------------------------------*/
       togglebox_w = XtVaCreateWidget("togglebox",
@@ -386,6 +468,16 @@ select_dir_dialog(Widget w, XtPointer client_data, XtPointer call_data)
 }
 
 
+/*++++++++++++++++++++++++++++++ toggled() ++++++++++++++++++++++++++++++*/
+void
+toggled(Widget w, XtPointer client_data, XtPointer call_data)
+{
+   toggles_set ^= (XT_PTR_TYPE)client_data;
+
+   return;
+}
+
+
 /*++++++++++++++++++++++++++ select_callback() ++++++++++++++++++++++++++*/
 static void
 select_callback(Widget w, XtPointer client_data, XtPointer call_data)
@@ -449,74 +541,81 @@ search_select_dir(Widget w, XtPointer client_data, XtPointer call_data)
 
    for (i = 0; i < no_of_dirs; i++)
    {
-      if (dirname_type == ALIAS_NAME)
+      if (((fra[i].protocol == LOC) && (toggles_set & SHOW_FILE)) ||
+          ((fra[i].protocol == FTP) && (toggles_set & SHOW_FTP)) ||
+          ((fra[i].protocol == SFTP) && (toggles_set & SHOW_SFTP)) ||
+          ((fra[i].protocol == HTTP) && (toggles_set & SHOW_HTTP)) ||
+          ((fra[i].protocol == EXEC) && (toggles_set & SHOW_EXEC)))
       {
-         match = pmatch((text[0] == '\0') ? "*" : text, connect_data[i].dir_alias, NULL);
-      }
-      else
-      {
-         match = pmatch((text[0] == '\0') ? "*" : text, fra[i].url, NULL);
-      }
-      if (match == 0)
-      {
-         if (deselect == YES)
+         if (dirname_type == ALIAS_NAME)
          {
-            if (connect_data[i].inverse == STATIC)
-            {
-               no_selected_static--;
-               draw_selection = YES;
-            }
-            else if (connect_data[i].inverse == ON)
-                 {
-                    no_selected--;
-                    draw_selection = YES;
-                 }
-                 else
-                 {
-                    draw_selection = NO;
-                 }
-            connect_data[i].inverse = OFF;
+            match = pmatch((text[0] == '\0') ? "*" : text, connect_data[i].dir_alias, NULL);
          }
          else
          {
-            if (static_select == YES)
+            match = pmatch((text[0] == '\0') ? "*" : text, fra[i].url, NULL);
+         }
+         if (match == 0)
+         {
+            if (deselect == YES)
             {
                if (connect_data[i].inverse == STATIC)
                {
-                  draw_selection = NO;
+                  no_selected_static--;
+                  draw_selection = YES;
+               }
+               else if (connect_data[i].inverse == ON)
+                    {
+                       no_selected--;
+                       draw_selection = YES;
+                    }
+                    else
+                    {
+                       draw_selection = NO;
+                    }
+               connect_data[i].inverse = OFF;
+            }
+            else
+            {
+               if (static_select == YES)
+               {
+                  if (connect_data[i].inverse == STATIC)
+                  {
+                     draw_selection = NO;
+                  }
+                  else
+                  {
+                     if (connect_data[i].inverse == ON)
+                     {
+                        no_selected--;
+                     }
+                     no_selected_static++;
+                     connect_data[i].inverse = STATIC;
+                     draw_selection = YES;
+                  }
                }
                else
                {
                   if (connect_data[i].inverse == ON)
                   {
-                     no_selected--;
+                     draw_selection = NO;
                   }
-                  no_selected_static++;
-                  connect_data[i].inverse = STATIC;
-                  draw_selection = YES;
-               }
-            }
-            else
-            {
-               if (connect_data[i].inverse == ON)
-               {
-                  draw_selection = NO;
-               }
-               else
-               {
-                  if (connect_data[i].inverse == STATIC)
+                  else
                   {
-                     no_selected_static--;
+                     if (connect_data[i].inverse == STATIC)
+                     {
+                        no_selected_static--;
+                     }
+                     no_selected++;
+                     connect_data[i].inverse = ON;
+                     draw_selection = YES;
                   }
-                  no_selected++;
-                  connect_data[i].inverse = ON;
-                  draw_selection = YES;
                }
             }
-         }
-         if (draw_selection == YES)
-         {
-            draw_dir_line_status(i, 1);
+            if (draw_selection == YES)
+            {
+               draw_dir_line_status(i, 1);
+            }
          }
       }
    }
